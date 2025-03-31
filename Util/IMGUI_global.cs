cdc653b95d29810e34d4d2d328c99bb6b4ac4c16
ram16 <thomass.montaine@gmail.com>
2025-03-31 17:54:49 +0200

    IMGUI_global

---
+++ Util/IMGUI_global.cs
@@ -0,0 +1,53 @@
+﻿using _UTIL_;
+using UnityEngine;
+
+namespace _SGUI_
+{
+    public sealed class IMGUI_global : MonoBehaviour
+    {
+        public interface IUser
+        {
+            void OnOnGUI();
+        }
+
+        public static IMGUI_global instance;
+
+        public readonly ListListener<IUser> users = new();
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void OnAfterSceneLoad()
+        {
+            Util.InstantiateOrCreateIfAbsent<IMGUI_global>();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            instance = this;
+            users.Reset();
+            DontDestroyOnLoad(gameObject);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Start()
+        {
+            users.AddListener1(isNotEmpty =>
+            {
+                if (gameObject != null)
+                    gameObject.SetActive(isNotEmpty);
+            });
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void OnGUI()
+        {
+            for (int i = 0; i < users._list.Count; i++)
+                users._list[i].OnOnGUI();
+        }
+    }
+}
