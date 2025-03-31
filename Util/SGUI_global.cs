e833279fe380254b12ea368a47588358b30ff84d
ram16 <thomass.montaine@gmail.com>
2025-04-01 01:52:55 +0200

    SGUI_global

---
+++ Util/SGUI_global.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace _SGUI_
+{
+    public class SGUI_global : MonoBehaviour
+    {
+        public static SGUI_global instance;
+
+        public Canvas canvas;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void OnBeforeSceneLoad()
+        {
+            Util.InstantiateOrCreateIfAbsent<SGUI_global>();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected virtual void Awake()
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            canvas = GetComponent<Canvas>();
+        }
+    }
+}
