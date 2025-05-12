03297fc90b3a2afb6b12876372ef0bbb927e6c8e
ram16 <thomass.montaine@gmail.com>
2025-05-13 01:21:11 +0200

    GIGA UPDATE

--- Runtime/SguiCustom/_JSonEditor.cs
+++ Runtime/SguiCustom/_JSonEditor.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using _ARK_;
+using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using UnityEngine;
+using static TMPro.TMP_InputField;
 
 namespace _SGUI_
 {
@@ -18,10 +19,21 @@ namespace _SGUI_
             trad_title.SetTrad(title ?? file_name);
         }
 
-        public void EditJSon(in object target)
+        public void EditJSon(in object json)
         {
-            Type target_type = target.GetType();
+            ArkJSon arkjson = json as ArkJSon;
+            trad_title.SetTrad(Path.GetFileName(arkjson.GetFilePath()));
+
+            Type target_type = arkjson.GetType();
             FieldInfo[] target_fields = target_type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            Action on_save = null;
+            onAction_confirm += () =>
+            {
+                on_save?.Invoke();
+                arkjson.SaveArkJSon(true);
+                return true;
+            };
 
             for (int i = 0; i < target_fields.Length; ++i)
             {
@@ -31,33 +43,70 @@ namespace _SGUI_
                     continue;
 
                 Type type = field.FieldType;
-                object value = field.GetValue(target);
+                object value = field.GetValue(arkjson);
                 string name = field.Name;
 
                 SguiCustom_Abstract button = null;
 
                 switch (value)
                 {
-                    case string str:
-                        var input_field = AddButton<SguiCustom_InputField>();
-                        input_field.input_field.text = str;
-                        button = input_field;
+                    case bool _bool:
+                        {
+                            var toggle = AddButton<SguiCustom_Toggle>();
+                            button = toggle;
+                            toggle.toggle.isOn = _bool;
+                            on_save += () => field.SetValue(arkjson, toggle.toggle.isOn);
+                        }
                         break;
 
-                    case bool b:
-                        var toggle = AddButton<SguiCustom_Toggle>();
-                        toggle.toggle.isOn = b;
-                        button = toggle;
+                    case int _int:
+                        {
+                            var input_int = AddButton<SguiCustom_InputField>();
+                            button = input_int;
+                            input_int.input_field.text = _int.ToString();
+                            input_int.input_field.contentType = ContentType.IntegerNumber;
+                            on_save += () => field.SetValue(arkjson, int.Parse(input_int.input_field.text));
+                        }
                         break;
-                }
 
-                if(false)
-                switch (type)
-                {
-                    case Type t when t == typeof(string):
-                        var input_field = AddButton<SguiCustom_InputField>();
-                        input_field.input_field.text = value.ToString();
-                        button = input_field;
+                    case float _float:
+                        {
+                            var input_float = AddButton<SguiCustom_InputField>();
+                            button = input_float;
+                            input_float.input_field.text = _float.ToString();
+                            input_float.input_field.contentType = ContentType.DecimalNumber;
+                            on_save += () => field.SetValue(arkjson, float.Parse(input_float.input_field.text));
+                        }
+                        break;
+
+                    case Enum _enum:
+                        {
+                            var dropdown = AddButton<SguiCustom_Dropdown>();
+                            button = dropdown;
+                            dropdown.dropdown.ClearOptions();
+                            dropdown.dropdown.AddOptions(Enum.GetNames(type).ToList());
+
+                            int enum_i = Convert.ToInt32(_enum);
+                            dropdown.dropdown.value = Enum.GetValues(type).Cast<int>().ToList().IndexOf(enum_i);
+                            dropdown.dropdown.RefreshShownValue();
+
+                            on_save += () =>
+                            {
+                                int index = dropdown.dropdown.value;
+                                string enum_name = dropdown.dropdown.options[index].text;
+                                field.SetValue(arkjson, Enum.Parse(type, enum_name));
+                            };
+                        }
+                        break;
+
+                    case string _str:
+                        {
+                            var input_str = AddButton<SguiCustom_InputField>();
+                            button = input_str;
+                            input_str.input_field.text = _str;
+                            input_str.input_field.contentType = ContentType.Standard;
+                            on_save += () => field.SetValue(arkjson, input_str.input_field.text);
+                        }
                         break;
                 }
 
