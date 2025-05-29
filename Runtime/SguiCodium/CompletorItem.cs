c23ea8f021e748f5cd9822fe74879caf1e11fb70
ram16 <thomass.montaine@gmail.com>
2025-05-29 16:08:16 +0200

    continuer intellisense

--- Runtime/SguiCodium/CompletorItem.cs
+++ Runtime/SguiCodium/CompletorItem.cs
@@ -1,17 +1,24 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _SGUI_
 {
     internal class CompletorItem : MonoBehaviour
     {
+        RawImage background;
         public TextMeshProUGUI label;
 
         //--------------------------------------------------------------------------------------------------------------
 
         private void Awake()
         {
+            background = GetComponent<RawImage>();
             label = transform.Find("label").GetComponent<TextMeshProUGUI>();
         }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+
     }
 }
