94260d2fe1abb06c99849c695ac7d50102dfa505
ram16 <thomass.montaine@gmail.com>
2025-11-28 19:00:54 +0100

    yes

---
+++ Runtime/SguiTerminal/SguiTerminal.cs
@@ -0,0 +1,11 @@
+﻿namespace _SGUI_
+{
+    public abstract partial class SguiTerminal : SguiWindow1
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            trad_title.SetTrad("SguiTerminal");
+        }
+    }
+}
