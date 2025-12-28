eb76edaaeb297fdcd1e42751c94b451b39436d99
ram16 <thomass.montaine@gmail.com>
2025-12-28 23:49:12 +0100

    searchbox

---
+++ Runtime/ContextTools/SguiContextSearchbar/SearchboxItem.cs
@@ -0,0 +1,26 @@
+﻿using _ARK_;
+using TMPro;
+using UnityEngine.UI;
+
+namespace _SGUI_.searchbox
+{
+    public sealed class SearchboxItem : ArkComponent
+    {
+        public SguiSearchbox searchbox;
+        public Button button;
+        public TextMeshProUGUI label;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected override void Awake()
+        {
+            searchbox = GetComponentInParent<SguiSearchbox>(true);
+            button = GetComponentInChildren<Button>();
+            label = GetComponentInChildren<TextMeshProUGUI>();
+
+            base.Awake();
+
+            button.onClick.AddListener(searchbox.Close);
+        }
+    }
+}
---
+++ Runtime/ContextTools/SguiContextSearchbar/SguiSearchbox.cs
@@ -0,0 +1,134 @@
+﻿using _ARK_;
+using _SGUI_.searchbox;
+using _UTIL_;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace _SGUI_
+{
+    public sealed partial class SguiSearchbox : ArkComponent
+    {
+        public static SguiSearchbox instance;
+
+        [SerializeField] CanvasGroup canvasGroup;
+        RectTransform rt, content_rt;
+        VerticalLayoutGroup vlayout;
+        public TMP_InputField input_search;
+        [SerializeField] SearchboxItem prefab_item;
+
+        readonly List<SearchboxItem> clones = new();
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected override void Awake()
+        {
+            instance = this;
+
+            base.Awake();
+
+            canvasGroup = GetComponent<CanvasGroup>();
+            rt = (RectTransform)transform.Find("rT");
+            content_rt = (RectTransform)transform.Find("rT/list/scrollview/viewport-mask/content");
+            vlayout = content_rt.Find("layout").GetComponent<VerticalLayoutGroup>();
+            input_search = transform.Find("rT/searchbar/inputfield").GetComponent<TMP_InputField>();
+            prefab_item = vlayout.GetComponentInChildren<SearchboxItem>(true);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            canvasGroup.alpha = 0;
+            NUCLEOR.delegates.LateUpdate += RefreshAlpha;
+        }
+
+        protected override void OnDisable()
+        {
+            NUCLEOR.delegates.LateUpdate -= RefreshAlpha;
+            base.OnDisable();
+            input_search.text = string.Empty;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected override void Start()
+        {
+            base.Start();
+
+            transform.Find("background").GetComponent<PointerClickHandler>().onClick += eventData =>
+            {
+                switch (eventData.button)
+                {
+                    case PointerEventData.InputButton.Left:
+                        Close();
+                        break;
+
+                    case PointerEventData.InputButton.Right:
+                        rt.position = eventData.position;
+                        break;
+                }
+            };
+
+            transform.Find("rT/button-close").GetComponent<Button>().onClick.AddListener(Close);
+
+            transform.Find("rT/searchbar/inputfield/clear-icon").GetComponent<Button>().onClick.AddListener(() => input_search.text = string.Empty);
+
+            prefab_item.gameObject.SetActive(false);
+
+            Close();
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        void RefreshAlpha()
+        {
+            float a = Mathf.MoveTowards(canvasGroup.alpha, 1, 7 * Time.deltaTime);
+            canvasGroup.alpha = a;
+            if (a >= 1)
+                NUCLEOR.delegates.LateUpdate -= RefreshAlpha;
+        }
+
+        public void AutoSize() => Util.AddAction(ref NUCLEOR.delegates.LateUpdate_onEndOfFrame_once, _AutoSize_Immediate);
+        internal void _AutoSize_Immediate()
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)vlayout.transform);
+            float h = vlayout.preferredHeight;
+            content_rt.sizeDelta = new Vector2(0, h);
+        }
+
+        public void Close() => Toggle(false);
+        void Toggle(in bool toggle)
+        {
+            foreach (var item in clones)
+                Destroy(item.gameObject);
+            clones.Clear();
+            gameObject.SetActive(toggle);
+        }
+
+        public void OpenHere(in Vector2 screenPosition)
+        {
+            Toggle(true);
+            rt.position = screenPosition;
+        }
+
+        public SearchboxItem AddItem()
+        {
+            var clone = prefab_item.Clone(true);
+            clones.Add(clone);
+            AutoSize();
+            return prefab_item;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        protected override void OnDestroy()
+        {
+            NUCLEOR.delegates.LateUpdate -= RefreshAlpha;
+            base.OnDestroy();
+        }
+    }
+}
