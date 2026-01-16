8e241db6dee08e76b9263f7d2329ff7bc4e9116f
ram16 <thomass.montaine@gmail.com>
2026-01-16 17:58:34 +0100

    context hover handler

---
+++ Runtime/ContextTools/SguiContextHover/ContextHoverHandler.cs
@@ -0,0 +1,11 @@
+﻿using _ARK_;
+using UnityEngine;
+
+namespace _SGUI_.context_hover
+{
+    public sealed class ContextHoverHandler : MonoBehaviour, SguiContextHover.IUser
+    {
+        public Traductions hover_infos;
+        Traductions SguiContextHover.IUser.OnSguiContextHover() => hover_infos;
+    }
+}
