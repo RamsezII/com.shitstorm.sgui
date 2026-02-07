ef26dc3bff1ebd1e2d1ceaf569c86057130b27a8
ram16 <thomass.montaine@gmail.com>
2026-02-07 01:50:01 +0100

    vers color selector

---
+++ Runtime/SguiColorPrompt/ColorPrompGradient.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _SGUI_.prompts.color_prompt
+{
+    internal sealed class ColorPromptGradient : MonoBehaviour
+    {
+        [SerializeField] SguiColorPrompt prompt;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            prompt = GetComponentInParent<SguiColorPrompt>(true);
+        }
+    }
+}
---
+++ Runtime/SguiColorPrompt/SguiColorPrompt.cs
@@ -0,0 +1,43 @@
+﻿using _UTIL_;
+using System;
+using UnityEngine;
+
+namespace _SGUI_
+{
+    public sealed partial class SguiColorPrompt : MonoBehaviour
+    {
+        static SguiColorPrompt instance;
+
+        Action<Color> onSubmit;
+        Action onCancel;
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            instance = this;
+            transform.Find("background").GetComponent<PointerClickHandler>().onClick += eventData =>
+            {
+                gameObject.SetActive(false);
+                onCancel?.Invoke();
+                onSubmit = null;
+                onCancel = null;
+            };
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static void RequestColor(in Action<Color> onSubmit, in Action onCancel)
+        {
+            instance.onSubmit = onSubmit;
+            instance.gameObject.SetActive(true);
+        }
+    }
+}
