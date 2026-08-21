708aac0376ac220e69a161a1219e073fe53b2b48
ram16 <thomass.montaine@gmail.com>
2026-08-21 17:59:32 +0200

    Update CmdTests.cs

--- Tests/Runtime/CmdTests.cs
+++ Tests/Runtime/CmdTests.cs
@@ -10,34 +10,30 @@ namespace _SGUIS_.Tests
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void OnAfterSceneLoad()
         {
-            TermServer.root_commands.AddCommand(new(
-                name: "test_progress",
-                owner: null,
-                execution: (reader, context) =>
+            TermServer.root_namespace.AddCommand("test_progress", static (reader, context) =>
+            {
+                while (reader.TryRead(out string read))
+                    if (float.TryParse(read, out float time) && time > 0)
+                        context.list_args.Add(time);
+
+                return (CmdExecution)ERoutine;
+                static IEnumerator<CmdStep> ERoutine(CmdContext context)
                 {
-                    while (reader.TryRead(out string read))
-                        if (float.TryParse(read, out float time) && time > 0)
-                            context.list_args.Add(time);
-
-                    return (CmdExecution)ERoutine;
-                    static IEnumerator<CmdStep> ERoutine(CmdContext context)
+                    for (int i = 0; i < context.list_args.Count; i++)
                     {
-                        for (int i = 0; i < context.list_args.Count; i++)
-                        {
-                            float value = (float)context.list_args[i];
-                            yield return CmdStep.Status($"{i}: {value} seconds.");
+                        float value = (float)context.list_args[i];
+                        yield return CmdStep.Status($"{i}: {value} seconds.");
 
-                            float timer = 0;
-                            while (timer < value)
-                            {
-                                timer += Time.unscaledDeltaTime;
-                                yield return default;
-                            }
+                        float timer = 0;
+                        while (timer < value)
+                        {
+                            timer += Time.unscaledDeltaTime;
+                            yield return default;
                         }
-                        yield return CmdStep.Result($"Done.");
                     }
+                    yield return CmdStep.Result($"Done.");
                 }
-            ));
+            });
         }
     }
 }
